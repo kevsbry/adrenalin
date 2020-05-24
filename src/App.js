ec20e93b475a02d1dbd60650cac9b6d67cb38af9
kevin <nivekbryan09.projects@gmail.com>
2020-05-24 12:35:58 +0800

    routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from "react";
 import "./App.css";
-import { Route, Switch, HashRouter } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Listing from "./pages/Listing";
 import Detail from "./pages/Datail";
 
 function App() {
   return (
     <div className="App">
-      <HashRouter basename="/">
-        <Switch>
-          <Route exact path="/" component={Listing} />
-          <Route path="/detail/:slug" component={Detail} />
-        </Switch>
-      </HashRouter>
+      <Switch>
+        <Route exact path="/" component={Listing} />
+        <Route path="/detail/:slug" component={Detail} />
+      </Switch>
     </div>
   );
 }
